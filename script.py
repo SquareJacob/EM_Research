e376e063cbab1b535ce46f3e72502241b428a93c
SquareJacob <jacobascherzer@gmail.com>
2025-08-21 19:58:36 -0400

    script

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 from FDTD.grid_comparison_torch import full_test
 #Boundary, Solution, Simulation Type
 trials = [[b, s + 1, 1] for b in ["PEC", "Periodic"] for s in range (5)]
+trials.pop(2)
+trials.pop(0)
 for trial in trials:
     full_test(trial[0], trial[1], 50, [512, 512, 512], trial[2], False, 1e-4, None, 1e-3)
