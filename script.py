dc9e3fc661251a60655f862f878ff8fb767d5d09
SquareJacob <jacobascherzer@gmail.com>
2025-09-18 16:37:47 -0400

    another one

--- script.py
+++ script.py
@@ -1,3 +1,3 @@
 from FDTD.grid_comparison_torch import full_test
 from FDTD.multi_test import multi_test
-multi_test('PEC', 2, 4, [128, 128, 512], False, [{'error': 1e-4, 'zero': 0, 'type': 0}, {'error': 1e-4, 'zero': 0, 'type': 1}, {'error': 1e-4, 'zero':1e-4, 'type': 1}, {'error': 1e-4, 'zero':1e-3, 'type': 1}], [0, 15], ignore_error = False)
+multi_test('PEC', 2, 8, [128, 128, 512], False, [{'error': 1e-4, 'zero': 0, 'type': 0}, {'error': 1e-4, 'zero': 0, 'type': 1}, {'error': 1e-4, 'zero':1e-4, 'type': 1}, {'error': 1e-4, 'zero':1e-3, 'type': 1}], [0, 15], ignore_error = False)
