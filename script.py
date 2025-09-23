fdf1fd201f4257d80b5c25ae1f8d73a40c6093ff
SquareJacob <jacobascherzer@gmail.com>
2025-09-23 14:23:40 -0400

    trial

--- script.py
+++ script.py
@@ -1,3 +1,3 @@
 from FDTD.grid_comparison_torch import full_test
 from FDTD.multi_test import multi_test
-multi_test('Periodic', 3, 10, [4, 256], False, [{'error': 1e-3, 'zero': 1e-3, 'type': 0}, {'error': 1e-4, 'zero': 0, 'type': 1}, {'error': 1e-4, 'zero': 1e-4, 'type': 1}, {'error': 1e-4, 'zero': 1e-3, 'type': 1}, {'error': 1e-3, 'zero': 1e-3, 'type': 1}], param = 80)
+multi_test('Periodic', 3, 10, [512], False, [{'error': 1e-3, 'zero': 1e-3, 'type': 0}, {'error': 1e-4, 'zero': 0, 'type': 1}, {'error': 1e-4, 'zero': 1e-4, 'type': 1}, {'error': 1e-4, 'zero': 1e-3, 'type': 1}, {'error': 1e-3, 'zero': 1e-3, 'type': 1}], param = 80)
