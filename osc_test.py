66f543e389539b21c529c85605000a29bbe40b15
SquareJacob <jacobascherzer@gmail.com>
2025-09-25 13:16:05 -0400

    test

--- osc_test.py
+++ osc_test.py
@@ -7,7 +7,7 @@ world_size = int(os.environ["WORLD_SIZE"])
 print(f"Running example on {rank} in a world with {world_size}", flush = True)
 
 device_mesh = init_device_mesh("cuda", (4,))
-x = DTensor(torch.rand(100, 100, device = "cuda"), device_mesh=device_mesh, placements = [Shard(0)])
+x = DTensor(torch.rand(100, 100, device = "cuda"), Shard = 0)
 print(x, flush = True)
 print(x.shape, flush = True)
 print(x + x.T, flush = True)
