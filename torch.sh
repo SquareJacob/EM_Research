674010b61c99b40973c0dd41b8cd345dc6aec973
SquareJacob <jacobascherzer@gmail.com>
2025-09-25 13:29:57 -0400

    oops

--- torch.sh
+++ torch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=one_comparison
 #SBATCH --account=PAS0061
-#SBATCH --time=1:00
+#SBATCH --time=3:00
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=256G
 #SBATCH --gpus-per-node=4
 
 source activate trains
-python --nproc_per_node=4 script.py
+torchrun --nproc_per_node=4 script.py
