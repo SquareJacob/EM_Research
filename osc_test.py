47ab941d5470e88e52133b7d6176092a43dae483
SquareJacob <jacobascherzer@gmail.com>
2025-09-25 12:38:54 -0400

    test

--- osc_test.py
+++ osc_test.py
@@ -1,6 +1,11 @@
+import os
 import torch
 from torch.distributed.tensor import DTensor, Shard, Replicate, distribute_tensor, distribute_module, init_device_mesh
 
-device_mesh = init_device_mesh("cuda", (4,))
+rank = int(os.environ["RANK"])
+world_size = int(os.environ["WORLD_SIZE"])
+print(f"Running example on {rank} in a world with {world_size}", flush = True)
+
+device_mesh = init_device_mesh("cuda", (1,4))
 x = distribute_tensor(torch.rand(100, 100, device = "cuda"), device_mesh=device_mesh)
 print(x, flush = True)
