086d0fc5e5a85899ea3e37e9112868149f6bea25
SquareJacob <jacobascherzer@gmail.com>
2025-09-25 12:27:19 -0400

    test

--- osc_test.py
+++ osc_test.py
@@ -1,6 +1,6 @@
 import torch
 from torch.distributed.tensor import DTensor, Shard, Replicate, distribute_tensor, distribute_module, init_device_mesh
 
-device_mesh = init_device_mesh("cuda", (1,))
-x = distribute_tensor(torch.rand(100, 100, device = "cuda"))
+device_mesh = init_device_mesh("cuda", (4,))
+x = distribute_tensor(torch.rand(100, 100, device = "cuda"), device_mesh=device_mesh)
 print(x, flush = True)
