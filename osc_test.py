8bf588d461e23e84d89b67dce570752d93cfab41
SquareJacob <jacobascherzer@gmail.com>
2025-09-25 12:58:04 -0400

    test

--- osc_test.py
+++ osc_test.py
@@ -6,7 +6,7 @@ rank = int(os.environ["RANK"])
 world_size = int(os.environ["WORLD_SIZE"])
 print(f"Running example on {rank} in a world with {world_size}", flush = True)
 
-device_mesh = init_device_mesh("cuda", (1,4))
+device_mesh = init_device_mesh("cuda", (4,))
 x = distribute_tensor(torch.rand(100, 100, device = "cuda"), device_mesh=device_mesh, placements = [Shard(0)])
 print(x, flush = True)
 torch.distributed.destroy_process_group()
