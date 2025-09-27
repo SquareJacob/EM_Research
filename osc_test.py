fca9d2cb48e0bbf622ccda2bba3d31249493c48b
SquareJacob <jacobascherzer@gmail.com>
2025-09-26 21:41:15 -0400

    oops

--- osc_test.py
+++ osc_test.py
@@ -3,7 +3,7 @@ import numpy as np
 x = np.linspace(0, np.pi, 1024)
 A = []
 for i in range(6):
-    A.append(np.zeros(1024, 1024, 1024), dtype = np.float64)
+    A.append(np.zeros((1024, 1024, 1024), dtype = np.float64))
 for i in range(10):
     for j in range(6):
         A[j] += np.sin(i * x[:, None, None]) * np.sin(i * x[None, :, None]) * np.sin(i * x[None, None, :])
