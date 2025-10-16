d0fd72c5956f52ce0b7623c0e39a453d716ac636
Jacob Scherzer <jacob123@pitzer-rw01.ten.osc.edu>
2025-10-16 18:19:55 -0400

    change

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 from FDTD.grid_comparison_torch import full_test
 from FDTD.multi_test import multi_test
 import numpy as np
-multi_test('Periodic', 3, 10, [8, 512], False, 
+multi_test('Periodic', 6, 10, [8, 512], False, 
            [{'error': 1e-4, 'zero': 1e-4, 'type': 0}] + 
            [{'error': float(f'1e{-i}'), 'zero': 0, 'type': 1} for i in range(3, 7)] +
            [{'error': float(f'1e{-j}'), 'zero': float(f'1e{-i}'), 'type': 1} for i in range(3, 7) for j in range(3, 7)], 
-           param =  20, mu = 1, eps = 1, start_t = 0.2)
+           param = [2,2], mu = 1, eps = 1, start_t = 0.2)
