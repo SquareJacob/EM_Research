0b81e3aa8e51dc67ffa39c00017d73e8b86eff20
SquareJacob <jacobascherzer@gmail.com>
2025-10-25 13:34:53 -0400

    trial

--- script.py
+++ script.py
@@ -3,4 +3,4 @@ from FDTD.multi_test import multi_test
 import numpy as np
 multi_test('PEC', 1, 3, [16, 512], False, 
            [{'error': 1e-7, 'zero': 1e-7, 'type': 0}, {'error': 1e-7, 'zero': 0, 'type': 1}, {'error': 1e-7, 'zero': 1e-7, 'type': 1}], 
-           param = 1, mu = 1, eps = 1, start_t = 0, noise = [0, 20])
+           param = 1, mu = 1, eps = 1, start_t = 0, noise = [20, 20])
