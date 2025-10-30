a659c25e459d6c1deba7badaf610b37beaca313b
Jacob Scherzer <jacob123@pitzer-rw01.ten.osc.edu>
2025-10-30 10:19:08 -0400

    transfer

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 from FDTD.grid_comparison_torch import full_test
 from FDTD.multi_test import multi_test
 import numpy as np
-multi_test('PEC', 1, 3, [16, 512], False, 
+multi_test('Periodic', 1, 3, [16, 512], False, 
            [j for i in [1e-4, 1e-5, 1e-6, 1e-7] for j in
             [{'error': i, 'zero': 0, 'type': 0}, {'error': i, 'zero': 0, 'type': 1}, {'error': i, 'zero': i, 'type': 1}]], 
-           param = 1, mu = 1, eps = 1, start_t = 0, noise = [0, 20])
+           param = 1, mu = 1, eps = 1, start_t = 0, noise = [20, 20])
